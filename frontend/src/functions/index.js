cf07617e90f616d9c88578932725a1c85143b02e
Gustavo Silva Quieregato <quieregatog@alunos.utfpr.edu.br>
2021-03-17 15:36:43 -0300

    tentando inserir as imagens por meio da função fs ou FileReader, só que não foi possivel

--- frontend/src/functions/index.js
+++ frontend/src/functions/index.js
@@ -1,4 +1,6 @@
-  export const splitWord = (word: string) => {
+import fs from 'fs';
+
+export const splitWord = (word: string) => {
   let ext = '', name = '';
   ext = word.split('.')[1];
   name = word.substring(0, 7);
@@ -12,6 +14,8 @@
 export const splitWordFull = (word: string) => {
   let primary_name = word.split("_")[0];
 
+  primary_name = primary_name.split(" ")[0].concat(" ", primary_name.split(" ")[1]);
+
   return primary_name;
 }
 
@@ -19,4 +23,20 @@ export const splitWordEnd = (word: string) => {
   let secondary_name = word.split("_")[1];
 
   return secondary_name;
+}
+
+export const listenImages =  () => {
+
+  console.log(fs);
+
+  // fs.readFileSync('../../assets/imagens_clb', 'utf-8', (err, file) => {
+  //   if(err) throw err;
+  //   console.log(file);
+  // })
+
+  // readdir('../assets/image_clb', function(error, file){
+  //   if(error)
+  //     throw Error(error);
+  //   console.log(file);
+  // });
 }
